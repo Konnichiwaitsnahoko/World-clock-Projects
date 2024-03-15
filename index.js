383d175776e19f6a3331ccb3f7251851d76c43bf
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-03-15 16:46:40 -0700

    Added moment.js and moment-timezone libraries for handling date and time.Created a function updateClock to update the date, time, and AM/PM indicator for a given city.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  //Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement =
+    losAngelesElement.querySelector(".time .time-text");
+  let losAngelesCurrentAmPmElement =
+    losAngelesElement.querySelector(".time .am-pm");
+  let losAngelesTime = moment();
+  let losAngelesCurrentDate = losAngelesTime.format("dddd, MMMM Do");
+  let losAngelesCurrentTime = losAngelesTime.format("h:mm:ss");
+  let losAngelesCurrentAmPm = losAngelesTime.format("A");
+  losAngelesDateElement.innerHTML = losAngelesCurrentDate;
+  losAngelesTimeElement.innerHTML = losAngelesCurrentTime;
+  losAngelesCurrentAmPmElement.innerHTML = losAngelesCurrentAmPm;
 
+  //Tokyo
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time .time-text");
+  let tokyoCurrentAmPmElement = tokyoElement.querySelector(".time .am-pm");
+  let tokyoCurrentDate = tokyoTime.format("dddd, MMMM Do");
+  let tokyoCurrentTime = tokyoTime.format("h:mm:ss");
+  let tokyoCurrentAmPm = tokyoTime.format("A");
+  tokyoDateElement.innerHTML = tokyoCurrentDate;
+  tokyoTimeElement.innerHTML = tokyoCurrentTime;
+  tokyoCurrentAmPmElement.innerHTML = tokyoCurrentAmPm;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
